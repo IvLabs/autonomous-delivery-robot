b6a6960df6b33c53090f665583bce34a525817c8
Rajeshree Deotalu <rajeshreedeotaluvnit@gmail.com>
2019-12-13 05:45:41 -0800

    Update fyp.ino

--- fyp.ino
+++ fyp.ino
@@ -1,7 +1,9 @@
 
 #include <Servo.h>
+#include<Wire.h>
 Servo mys;
-
+const int MPU6050_addr=0x68;
+int16_t AccX,AccY,AccZ,Temp,GyroX,GyroY,GyroZ;
 int b_left1 = 6;
 int b_left2 = 7;
 int sp_pwm = 9;
@@ -16,6 +18,12 @@ pinMode(b_left2, OUTPUT);
 pinMode(sp_pwm, OUTPUT);
 Serial.begin(9600);
 mys.write(90);
+  Wire.begin();
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -72,5 +80,23 @@ void loop() {
   delay(1);   
 
   }
-
+ Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  //Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+ // Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+  delay(100);
 }
