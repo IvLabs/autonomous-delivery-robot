a33dce25381b01131f14948ca580a6a16bfa2f72
yogesh <yogeshphalak1998@gmail.com>
2020-01-26 07:18:12 +0530

    bot_steer(angle) -440<=angle<=440,i.e. -40 to 40 degrees.

---
+++ Controls/Stearing/Stearing.ino
@@ -0,0 +1,31 @@
+#include "DynamixelMotor.h"
+
+const long unsigned int baudrate=1000000;
+HardwareDynamixelInterface interface(Serial);
+DynamixelMotor motor(interface,0);
+
+void setup() {
+  interface.begin(baudrate);
+}
+
+void loop() {
+
+ for(int i=0;i<44;i++){
+  bot_stear(-440+20*i);
+ delay(500);
+ }
+ for(int i=0;i<44;i++){
+  bot_stear(440-20*i);
+ delay(500);
+ }
+
+}
+
+void bot_stear(long int ang){
+  motor.enableTorque();
+  motor.jointMode(1909,2791);
+  motor.speed(512);
+  long int pos;
+  pos = ang+2350;
+  motor.goalPosition(pos);
+}
