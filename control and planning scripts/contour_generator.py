f10b292d235807fa02e9c6922708bcda681f52f2
Unmesh <34339038+patilunmesh@users.noreply.github.com>
2020-03-04 00:52:07 +0530

    v_2

--- control and planning scripts/contour_generator.py
+++ control and planning scripts/contour_generator.py
@@ -9,7 +9,7 @@ input:: lidar scan
 		segmentation map image
 
 '''
-######################################################### imports
+######################################################### imports and globals
 
 import math
 import rospy, cv2
@@ -23,13 +23,14 @@ range_list = []
 segmap_contour = []
 merged_contour = []
 
-######################################################### vision functions
+######################################################### callacks
 
 def callback_scan_processor(data):
 	# the scan input is from -180 to 180 degrees. So total 360 degrees are covered in scan by 0.5 degree increment.
 	# total 720 readings for 360 degrees.
 	for i in range(len(data.ranges)):
 		range_list.append(data.ranges[i])
+	print(len(range_list))
 
 def callback_image_processor(data):
 	try:
@@ -38,16 +39,57 @@ def callback_image_processor(data):
         #image_sub.unregister()
     except CvBridgeError, e:
         print e
-
+    xs, ys, mask_blank = get_contours_list(camera_img)
     #further processing
+    for i in range(xs):
+    	point = xs[i], ys[i]
+    	segmap_contour.append(point) #list of touples
+
+######################################################### helping functions
+
+def get_contours_list(img):
+	bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	_, cnts, h = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	h = np.array(h).T.reshape(4,-1)
+	max_area = -1
+	index = -1
+	for i in range(len(cnts)):
+		area = cv2.contourArea(cnts[i])
+		if area > max_area:
+			max_area = area
+			index = i
+	indices = np.where(h[3] == index)
+	h,w = bw.shape	
+	blank = np.zeros((h+5, w), np.uint8)				
+	cv2.polylines(blank, pts=[cnts[index]], isClosed = True, color=255)				
+	for indice in indices:
+		if indice:
+			cv2.polylines(blank, pts=[cnts[indice]], isClosed = True,  color=255)					
+	mask = cv2.imread("sample.png", 0)		
+	ys, xs = np.where(mask & blank == 255)
+	mask_blank = mask & blank
+	return xs, ys, mask_blank
 
 
-def crop_contour(list_old):
+def crop_contour():
 	#lidar scan gives 720 readings starting from -pi to pi and the camera scan gives input for 58 degrees.
 	# it is 29 degrees left and 29 degree right of current orientation of bot.
 	curr_theta = curr_pose.position.z
-	start_index = (curr_theta + 29)*2 - 1 
-	end_index = (curr_theta - 29)*2 - 1
+	start = abs(int(curr_theta + 29)*2 - 1)
+	end = abs(int(curr_theta - 29)*2 - 1)
+	if start > end:
+		start, end = end, start
+	if not 0 < start, end < 720:
+		print('error cropping')
+		return 0
+	for i in range(0, start):
+		merged_contour[i] = range_list[i]
+	for i in range(0, len(segmap_contour)):
+		merged_contour[i + start] = segmap_contour[i]
+	for i in range(end, len(range_list)-1):
+		merged_contour[i + len(segmap_contour)] = range_list[i]
+	return merged_contour
+
 
 ######################################################### Main body
 if __name__ == '__main__':
